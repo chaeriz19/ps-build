2f663e38bdd3063ec500b1341d9f60b22167a399
Chris Ouboter <129173682+chaeriz19@users.noreply.github.com>
2024-07-02 21:22:49 +0200

    init

---
+++ assets/Endpoint-CEG5l7x9.js
@@ -0,0 +1,1 @@
+import{_ as e,f as t,g as o}from"./index-DgN9BqVy.js";const c={},n={class:"font-bold text-xl text-gray-700 dark:text-white"};function r(s,a){return t(),o("p",n," Gemaakt door: Chris, Givenchy & Wesley ")}const d=e(c,[["render",r]]);export{d as default};
